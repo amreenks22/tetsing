028a2a3fad26090d93e22fb08097ab2a59ccc23c
amreenks22 <36256079+amreenks22@users.noreply.github.com>
2018-04-06 15:29:45 +0530

    Update sc1.js

--- sc1.js
+++ sc1.js
@@ -5,32 +5,7 @@ function onChange(control, oldValue, newValue, isLoading) {
 	
 	//Type appropriate comment here, and begin script below
 	
-	if ((newValue == "New Account Request") || (newValue == "Update to an existing account")) {
-		var Person = Backbone.Model.extend({
-		
-		    defaults: {  // Noncompliant; every instance of Person will share the same instance of favoriteColors
-        favoriteColors: ["blue","purple","raspberry"]
-    }
-});
-		function foo() {
-			var myArray = ['a', 'b', 'c', 'd'];
-
-delete myArray[2];  // Noncompliant. myArray => ['a', 'b', undefined, 'd']
-console.log(myArray[2]); 
-  let x = bar(1);
-		
-  if (x > 0) {
-      let x = bar(2); // Noncompliant
-      console.log(x);
-	  var a=3
-	  if(a=4){
-		  a=a+1;
-	  }
-  } else {
-     console.log("Wrong Value");
-  }
-}
-		
+	
 		
 		g_form.setVisible('api_module', true);
 		
